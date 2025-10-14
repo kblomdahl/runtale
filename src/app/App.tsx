968ef5c859034accecb704addbfbfe3cf1704453
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2025-10-14 18:18:19 +0000

    Remove the "Race Predictor" tab

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import Paces from '../features/paces/Paces';
 import Zones from '../features/zones/Zones';
 import Sessions from '../features/sessions/Sessions';
 import Strength from '../features/strength/Strength';
-import RacePredictor from '../features/race-predictor/RacePredictor';
 import NorwegianSingles from '../features/norwegian-singles/NorwegianSingles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
@@ -15,7 +14,6 @@ function App() {
       <Route path="/" element={<Paces />} />
       <Route path="/zones" element={<Zones />} />
       <Route path="/sessions" element={<Sessions />} />
-      <Route path="/race-predictor" element={<RacePredictor />} />
       <Route path="/norwegian-singles" element={<NorwegianSingles />} />
       <Route path="/strength" element={<Strength />} />
     </Routes>
