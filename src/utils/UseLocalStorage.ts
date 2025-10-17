964dd6ae625854feb3f1c077ce70abe14f67deee
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2025-10-17 18:33:07 +0000

    Replace `reviver` with `parser` argument

--- src/utils/UseLocalStorage.ts
+++ src/utils/UseLocalStorage.ts
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-export default function useLocalStorage<T>(key: string, initialValue: T, reviver?: (value: string) => T): [T, (value: T) => void] {
+export default function useLocalStorage<T>(key: string, initialValue: T, parser?: (value: string) => T): [T, (value: T) => void] {
   const [storedValue, setStoredValue] = useState<T>(() => {
     const item = window.localStorage.getItem(key);
 
-    if (reviver) {
-      return item ? reviver(item) : initialValue;
+    if (parser) {
+      return item ? parser(item) : initialValue;
     } else {
       return item ? JSON.parse(item) as T : initialValue;
     }
