9532e7de35acff61282e9ae81bef6b565ca8debc
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2025-10-25 19:20:13 +0000

    Split styling specific to `NorwegianSingles`

--- src/features/norwegian-singles/NorwegianSingles.tsx
+++ src/features/norwegian-singles/NorwegianSingles.tsx
@@ -10,7 +10,8 @@ import DurationInputField from '../../components/DurationInputField';
 import CriticalSpeed from './CriticalSpeed';
 import useLocalStorage from '../../utils/UseLocalStorage';
 import { getNumberFromForm, getNumbersFromForm } from '../../utils/Form';
-import styles from '../../styles/shared.module.css';
+import sharedStyles from '../../styles/shared.module.css';
+import styles from './NorwegianSingles.module.css';
 
 const INPUT_TARGET_TRAINING_LOAD = 'targetTrainingLoad';
 const INPUT_LONG_RUN_DAY = 'longRunDay';
@@ -63,9 +64,9 @@ function NorwegianSingles() {
   };
 
   return <>
-    <form onSubmit={setTrainingParameters}>
-      <div className={styles.layout__responsive}>
-        <section className={styles.form}>
+    <form onSubmit={setTrainingParameters} className={styles.form}>
+      <div className={sharedStyles.layout__responsive}>
+        <section className={sharedStyles.form}>
           <b>
             Training Plan Parameters
           </b>
@@ -90,26 +91,26 @@ function NorwegianSingles() {
             </select>
           </label>
         </section>
-        <section className={styles.form}>
+        <section className={sharedStyles.form}>
           <b>
             Critical Speed from Time Trials
           </b>
           <label>
             <span>Time Trial 1</span>
-            <span className={styles.layout__cluster}>
+            <span className={sharedStyles.layout__cluster}>
               <input type='number' name={INPUT_DISTANCES} defaultValue={distances[0].toMeters()} placeholder={'1609.34'} width='40%' />
               <DurationInputField name={INPUT_TIMES} defaultValue={times[0]} />
             </span>
           </label>
           <label>
             <span>Time Trial 2</span>
-            <span className={styles.layout__cluster}>
+            <span className={sharedStyles.layout__cluster}>
               <input type='number' name={INPUT_DISTANCES} defaultValue={distances[1].toMeters()} placeholder={'5000'} width='40%' />
               <DurationInputField name={INPUT_TIMES} defaultValue={times[1]} />
             </span>
           </label>
         </section>
-        <button type='submit' className={styles.layout__full}>Plan</button>
+        <button type='submit' className={sharedStyles.layout__full}>Plan</button>
       </div>
     </form>
     <hr />
