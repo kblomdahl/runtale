5d3537aa06373731868cd701fa4bdaa830cc668f
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2025-10-25 18:19:19 +0000

    Fix indentation from tabs to spaces

--- src/utils/Form.ts
+++ src/utils/Form.ts
@@ -1,17 +1,17 @@
 export function getNumberFromForm(formData: FormData, key: string, defaultValue: number = 0): number {
-	const value = formData.get(key);
+  const value = formData.get(key);
 
   if (value === null || value === '') {
     return defaultValue;
   }
 
   const num = Number(value);
-	return isNaN(num) ? defaultValue : num;
+  return isNaN(num) ? defaultValue : num;
 }
 
 export function getNumbersFromForm(formData: FormData, key: string, defaultValue: number = 0): number[] {
-	return formData.getAll(key).map(value => {
-		const num = Number(value);
-		return isNaN(num) ? defaultValue : num;
-	});
+  return formData.getAll(key).map(value => {
+    const num = Number(value);
+    return isNaN(num) ? defaultValue : num;
+  });
 }
