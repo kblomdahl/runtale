fd1b3a88b9e1bbf8680bf4f25a95904b7f642084
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2025-10-25 17:52:34 +0000

    Use hash based routing to improve github pages compatibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ function Header() {
     <img src={logoUrl} height="36" alt="logo" />
     <nav>
       <ul>
-        <li><a href="/">Paces</a></li>
-        <li><a href="/zones">Zones</a></li>
-        <li><a href="/sessions">Sessions</a></li>
-        <li><a href="/strength">Strength</a></li>
-        <li><a href="/norwegian-singles">Norwegian Singles</a></li>
+        <li><a href="#/">Paces</a></li>
+        <li><a href="#/zones">Zones</a></li>
+        <li><a href="#/sessions">Sessions</a></li>
+        <li><a href="#/strength">Strength</a></li>
+        <li><a href="#/norwegian-singles">Norwegian Singles</a></li>
       </ul>
     </nav>
   </header>;
